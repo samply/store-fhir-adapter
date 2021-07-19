f0b906d9cff70603386b5d9dc3c028154ea7de2c
Patrick Skowronek <patrick.skowronek@dkfz-heidelberg.de>
2021-07-19 08:46:48 +0200

    Added Tumormapping
    And some small enchantment

---
+++ src/main/java/de/samply/store/adapter/fhir/service/mapping/TumorMapping.java
@@ -0,0 +1,50 @@
+package de.samply.store.adapter.fhir.service.mapping;
+
+import static de.samply.store.adapter.fhir.service.mapping.Util.DATE_STRING;
+import static de.samply.store.adapter.fhir.service.mapping.Util.LOCAL_DATE;
+import static de.samply.store.adapter.fhir.service.mapping.Util.lift2;
+
+import de.samply.share.model.ccp.Container;
+import org.hl7.fhir.r4.hapi.fluentpath.FhirPathR4;
+import org.hl7.fhir.r4.model.CodeType;
+import org.hl7.fhir.r4.model.Condition;
+import org.hl7.fhir.r4.model.DateTimeType;
+import org.hl7.fhir.r4.model.Patient;
+import org.hl7.fhir.r4.model.PrimitiveType;
+import org.hl7.fhir.r4.model.StringType;
+import org.hl7.fhir.r4.model.ValueSet;
+import org.springframework.stereotype.Component;
+
+/**
+ * Mapping of the Sample container.
+ *
+ * @author Patrick Skowronek
+ */
+
+@Component
+public class TumorMapping {
+
+  public static final String ICD_0_3 = "urn:oid:2.16.840.1.113883.6.43.1";
+  public static final String ADT_Site = "urn:oid:2.16.840.1.113883.2.6.60.7.1.1";
+
+  private final FhirPathR4 fhirPathR4;
+
+  public TumorMapping(FhirPathR4 fhirPathR4) {
+    this.fhirPathR4 = fhirPathR4;
+  }
+
+  public Container map(Condition condition) {
+    var builder = new ContainerBuilder(fhirPathR4, condition, "Tumor");
+
+    builder.addAttribute("Condition.bodySite.coding.where(system = '" + ICD_0_3 + "').code",
+        CodeType.class, "urn:dktk:dataelement:4:2", PrimitiveType::getValue);
+
+    builder.addAttribute("Condition.bodySite.coding.where(system = '" + ICD_0_3 + "').version",
+        StringType.class, "urn:dktk:dataelement:5:2", PrimitiveType::getValue);
+
+    builder.addAttribute("Condition.bodySite.coding.where(system = '" + ADT_Site + "').code",
+        CodeType.class, "urn:dktk:dataelement:6:2", PrimitiveType::getValue);
+
+    return builder.build();
+  }
+}
--- src/test/java/de/samply/store/adapter/fhir/service/mapping/SampleMappingTest.java
+++ src/test/java/de/samply/store/adapter/fhir/service/mapping/SampleMappingTest.java
@@ -10,14 +10,12 @@ import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvFileSource;
 
-import static de.samply.store.adapter.fhir.service.TestUtil.convertCsvValue;
 import static de.samply.store.adapter.fhir.service.TestUtil.findAttributeValue;
 import static org.junit.jupiter.api.Assertions.*;
 
 /**
  * @author Patrick Skowronek
  */
-
 class SampleMappingTest {
 
   private SampleMapping mapping;
@@ -29,83 +27,21 @@ class SampleMappingTest {
 
   @ParameterizedTest
   @CsvFileSource(resources = "/sampleMappings.csv", numLinesToSkip = 1)
-  void toUpperCase_ShouldGenerateTheExpectedUppercaseValueCSVFile(
-      String fhir_sample, String kind_value, String type_value, String fixing_value
+  void map_sampleMaterialTypeCSVFile(
+      String fhirSample, String kindValue, String typeValue, String fixingValue
   ) {
     var specimen = new Specimen();
     specimen.getType().getCodingFirstRep()
-        .setSystem("https://fhir.bbmri.de/CodeSystem/SampleMaterialType").setCode(fhir_sample);
+        .setSystem("https://fhir.bbmri.de/CodeSystem/SampleMaterialType").setCode(fhirSample);
 
     var container = mapping.map(specimen);
 
-    assertEquals(convertCsvValue(kind_value),
+    assertEquals(Optional.ofNullable(kindValue),
         findAttributeValue(container, "urn:dktk:dataelement:95:2"));
-    assertEquals(convertCsvValue(type_value),
+    assertEquals(Optional.ofNullable(typeValue),
         findAttributeValue(container, "urn:dktk:dataelement:97:1"));
-    assertEquals(convertCsvValue(fixing_value),
+    assertEquals(Optional.ofNullable(fixingValue),
         findAttributeValue(container, "urn:dktk:dataelement:90:1"));
-  }
-
-  @Test
-  void map_specimenTypeWholeBlood() {
-    var specimen = new Specimen();
-    specimen.getType().getCodingFirstRep()
-        .setSystem("https://fhir.bbmri.de/CodeSystem/SampleMaterialType").setCode("whole-blood");
-
-    var container = mapping.map(specimen);
-
-    assertEquals(Optional.of("Vollblut"),
-        findAttributeValue(container, "urn:dktk:dataelement:95:2"));
-    assertEquals(Optional.of("Flüssigprobe"),
-        findAttributeValue(container, "urn:dktk:dataelement:97:1"));
-    assertTrue(findAttributeValue(container, "urn:dktk:dataelement:90:1").isEmpty());
-  }
-
-  @Test
-  void map_specimenTypeTissueFfpe() {
-    var specimen = new Specimen();
-    specimen.getType().getCodingFirstRep()
-        .setSystem("https://fhir.bbmri.de/CodeSystem/SampleMaterialType").setCode("tissue-ffpe");
-
-    var container = mapping.map(specimen);
-
-    assertEquals(Optional.of("Tumorgewebe"),
-        findAttributeValue(container, "urn:dktk:dataelement:95:2"));
-    assertEquals(Optional.of("Gewebeprobe"),
-        findAttributeValue(container, "urn:dktk:dataelement:97:1"));
-    assertEquals(Optional.of("Paraffin (FFPE)"),
-        findAttributeValue(container, "urn:dktk:dataelement:90:1"));
-  }
-
-  @Test
-  void map_specimenTypeTissueFrozen() {
-    var specimen = new Specimen();
-    specimen.getType().getCodingFirstRep()
-        .setSystem("https://fhir.bbmri.de/CodeSystem/SampleMaterialType")
-        .setCode("tumor-tissue-frozen");
-
-    var container = mapping.map(specimen);
-
-    assertEquals(Optional.of("Tumorgewebe"),
-        findAttributeValue(container, "urn:dktk:dataelement:95:2"));
-    assertEquals(Optional.of("Gewebeprobe"),
-        findAttributeValue(container, "urn:dktk:dataelement:97:1"));
-    assertEquals(Optional.of("Kryo/Frisch (FF)"),
-        findAttributeValue(container, "urn:dktk:dataelement:90:1"));
-  }
-
-  @Test
-  void map_specimenTypePlasmaEdta() {
-    var specimen = new Specimen();
-    specimen.getType().getCodingFirstRep()
-        .setSystem("https://fhir.bbmri.de/CodeSystem/SampleMaterialType").setCode("plasma-edta");
-
-    var container = mapping.map(specimen);
-
-    assertEquals(Optional.of("Plasma"), findAttributeValue(container, "urn:dktk:dataelement:95:2"));
-    assertEquals(Optional.of("Flüssigprobe"),
-        findAttributeValue(container, "urn:dktk:dataelement:97:1"));
-    assertTrue(findAttributeValue(container, "urn:dktk:dataelement:90:1").isEmpty());
   }
 
   @Test
@@ -118,22 +54,4 @@ class SampleMappingTest {
     assertEquals(Optional.of("01.01.2000"),
         findAttributeValue(container, "urn:dktk:dataelement:49:4"));
   }
-
-
-  @Test
-  void map_specimenTypeError() {
-    var specimen = new Specimen();
-    specimen.getType().getCodingFirstRep()
-        .setSystem("https://fhir.bbmri.de/CodeSystem/SampleMaterialType")
-        .setCode("derivative-other");
-
-    Exception exception = assertThrows(IllegalArgumentException.class, () -> mapping.map(specimen));
-
-    String expectedMessage = "Not mapable value ";
-    String actualMessage = exception.getMessage();
-
-    assertTrue(actualMessage.contains(expectedMessage));
-  }
-
-
 }
