a6c0bba76690de8496a295aef5aa0fa47991a7df
Alexander Kiel <alexanderkiel@gmx.net>
2021-11-01 15:21:46 +0100

    Histology Focus Mapping

--- src/main/java/de/samply/store/adapter/fhir/model/ConditionNode.java
+++ src/main/java/de/samply/store/adapter/fhir/model/ConditionNode.java
@@ -2,6 +2,7 @@ package de.samply.store.adapter.fhir.model;
 
 import java.util.List;
 import org.hl7.fhir.r4.model.Condition;
+import org.hl7.fhir.r4.model.Observation;
 import org.hl7.fhir.r4.model.Patient;
 
 /**
@@ -9,9 +10,10 @@ import org.hl7.fhir.r4.model.Patient;
  * set.
  */
 public record ConditionNode(Patient patient, Condition condition,
+                            List<Observation> histologies,
                             List<ClinicalImpressionNode> clinicalImpressions) {
 
   public ConditionNode(Patient patient, Condition condition) {
-    this(patient, condition, List.of());
+    this(patient, condition, List.of(), List.of());
   }
 }
--- src/main/java/de/samply/store/adapter/fhir/model/ConditionNodeBuilder.java
+++ src/main/java/de/samply/store/adapter/fhir/model/ConditionNodeBuilder.java
@@ -1,15 +1,19 @@
 package de.samply.store.adapter.fhir.model;
 
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 import java.util.Objects;
 import java.util.stream.Stream;
 import org.hl7.fhir.r4.model.Condition;
+import org.hl7.fhir.r4.model.Observation;
 
 class ConditionNodeBuilder {
 
   private final PatientNodeBuilder patientNodeBuilder;
   private Condition condition;
+  private final List<Observation> histologies = new ArrayList<>();
   private final Map<String, ClinicalImpressionNodeBuilder> clinicalImpressionContainers =
       new HashMap<>();
 
@@ -21,6 +25,10 @@ class ConditionNodeBuilder {
     this.condition = Objects.requireNonNull(condition);
   }
 
+  void addHistology(Observation histology) {
+    histologies.add(histology);
+  }
+
   ClinicalImpressionNodeBuilder getClinicalImpressionNodeBuilder(String reference) {
     return clinicalImpressionContainers.computeIfAbsent(Objects.requireNonNull(reference),
         k -> new ClinicalImpressionNodeBuilder());
@@ -29,6 +37,7 @@ class ConditionNodeBuilder {
   Stream<ConditionNode> build() {
     return patientNodeBuilder.getPatient().stream().flatMap(patient -> Stream.ofNullable(condition)
         .map(condition -> new ConditionNode(patient, condition,
+            List.copyOf(histologies),
             clinicalImpressionContainers.values().stream()
                 .flatMap(ClinicalImpressionNodeBuilder::build)
                 .toList())));
